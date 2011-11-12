6cea7ea8cba2f07433d4c19dd789e816cd0cf6e6
Slotos <slotos@gmail.com>
2011-11-12 16:18:41 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeedboxRb::Application.config.secret_token = 'd5deee3870d08eb12c93b1577bd81499d93cbcab8fa7f91e81a81e5ebcaf36cfe1d6b29988da524802f3dae4f4e69aff3bab908a148d3ebfe378c74bdfde866b'
